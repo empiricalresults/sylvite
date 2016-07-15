e928f9290407cc4ba2b17ecd1822174e2c52d0ed
jonson <jdeyoung@gmail.com>
2016-07-14 17:07:42 -0700

    Add validateSequenceNumberBeforeCheckpointing support

--- src/main/java/com/er/sylvite/KCLConfig.java
+++ src/main/java/com/er/sylvite/KCLConfig.java
@@ -43,7 +43,8 @@ public class KCLConfig {
             "AWSCredentialsProvider",
             "workerId",
             "streamName",
-            "initialPositionInStream"
+            "initialPositionInStream",
+            "validateSequenceNumberBeforeCheckpointing"
     };
 
 
